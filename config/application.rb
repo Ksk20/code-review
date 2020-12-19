45faceb25b16ab919e4ffb543e5dba5f528386bf
ksk <ksk@ksknoMacBook-Pro.local>
2020-12-19 20:04:12 +0900

    fix

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Numare
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -19,4 +20,4 @@ module Numare
   end
 end
 
-config.assets.initialize_on_precompile = false
+
